143695b5465ebc255a879214f88976fa5af9474f
Prakul Sharma <176670611+ps215665@users.noreply.github.com>
2025-02-05 20:13:47 +0530

    changes

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
     @foreach( $jobs as $job )
-        <div class="mb-4 rounded-md border-spacing-1 border-gray-300 p-4">{{$job->title}}</div>
+        <div class="mb-4 rounded-md border-spacing-1 border-gray-300 p-4 text-blue-500">{{$job->title}}</div>
     @endforeach
-
 </x-layout>
