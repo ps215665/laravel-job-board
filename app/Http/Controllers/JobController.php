19fa70a6be05770860d3ced523c535d5bc4bb64b
Prakul Sharma <176670611+ps215665@users.noreply.github.com>
2025-02-07 13:04:13 +0530

    category filter

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -22,7 +22,9 @@ class JobController extends Controller
         })->when(request('max_salary'), function($query) {
             $query->where('salary', '<=', request('max_salary'));
         })->when(request('experience'), function($query) {
-            $query->where('experience', 'like', '%' . request('experience') . '%');
+            $query->where('experience', request('experience'));
+        })->when(request('category'), function($query) {
+            $query->where('category', request('category'));
         });
 
         return view('job.index', ['jobs' => $jobs->paginate(3)]);
