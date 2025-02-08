bd4413a016698d3034ace67162335ad10ab4a70c
Prakul Sharma <176670611+ps215665@users.noreply.github.com>
2025-02-08 21:31:22 +0530

    Add CV upload field and saving cvs localy

--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -6,11 +6,16 @@
         <h2 class="mb-4 text-lg font-medium">
             <span class="text-slate-700">Your Job Application</span>
         </h2>
-        <form action="{{ route('job.application.store', $job) }}" method="POST">
+        <form action="{{ route('job.application.store', $job) }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="mb-4">
-                <label for="expected_salary" class="mb-1"> Expected Salary</label>
+                <label for="expected_salary" class="mb-1">Expected Salary</label>
                 <x-text-input type="number" name="expected_salary" form-ref="application" />
+            </div>
+
+            <div class="mb-4">
+                <label for="expected_salary" class="mb-1">Upload CV</label>
+                <x-text-input type="file" name="cv" form-ref="application" />
             </div>
 
             <x-button class="w-full cursor-pointer">Apply</x-button>
