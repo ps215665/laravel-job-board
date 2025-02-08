c8a8c987b0c077599686476c4a034961eff0cae7
Prakul Sharma <176670611+ps215665@users.noreply.github.com>
2025-02-08 15:44:26 +0530

    job applications model and controller

---
+++ app/Http/Controllers/JobApplication.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class JobApplication extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Job $job)
+    {
+        Gate::authorize('apply', $job);
+
+        return view('job_application.create', ['job' => $job]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Job $job, Request $request)
+    {
+        $job->jobApplications()->create([
+            'user_id' => $request->user()->id,
+            ... $request->validate([
+                'expected_salary' => 'required|min:1|max:1000000'
+            ])
+        ]);
+
+        return redirect()->route('jobs.show', $job)
+            ->with('success', 'Job application submitted.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
