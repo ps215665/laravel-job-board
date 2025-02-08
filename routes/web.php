4f1e867c4a273a28d41780d5a463c2e1a852bc23
Prakul Sharma <176670611+ps215665@users.noreply.github.com>
2025-02-08 15:45:36 +0530

    routes changes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('', fn() => to_route('jobs.index'));
-Route::resource('jobs', \App\Http\Controllers\JobController::class)->only(['index']);
+Route::resource('jobs', \App\Http\Controllers\JobController::class)->only(['index', 'show']);
 
 Route::get('login', fn() => to_route('auth.create'))->name('login');
 Route::resource('auth', \App\Http\Controllers\AuthController::class)->only(['store', 'create']);
 
+Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
+Route::delete('auth', [\App\Http\Controllers\AuthController::class, 'destroy'])->name('auth.destroy');
+
+Route::middleware('auth')->group(function () {
+   Route::resource('job.application', \App\Http\Controllers\JobApplication::class)
+   ->only(['create', 'store', 'show']);
+});
